6f18bf340f799ba5c4d033b68bd1455f70ffbfa0
Madeline E Zechar <mez2113@kubrick.umbia.edu>
2019-04-17 13:00:19 -0400

    add term freq csv

--- sklearn_stopwords.py
+++ sklearn_stopwords.py
@@ -22,25 +22,7 @@ def main():
             	keras_doc += row[3] + ' '
             if row[2] == 'pytorch':
                 pytorch_doc += row[3] + ' '
-        '''
-        pytorch_doc = []
-        tensorflow_doc = []
-        cotag_list = []
-        keras_doc = []
-        counte = 0
-        for row in csv_reader:
-            if row[2] == 'keras':
-            	keras_doc.append(row[3])
-            if row[2] == 'pytorch':
-             	pytorch_doc.append(row[3])
-            if row[2] == 'tensorflow':
-             	tensorflow_doc.append(row[3])
-    print(keras_doc[0:10])
-    print("BREAK")
-    print(pytorch_doc[0:10])
-    print("BREAK")
-    print(tensorflow_doc[0:10])
-    '''
+
     corpus = [pytorch_doc, tensorflow_doc, keras_doc]
     vectorizer = CountVectorizer()
     x = vectorizer.fit_transform(corpus)
@@ -59,23 +41,14 @@ def main():
     print(sorted_arr[-10:])
     #print(vectorizer.get_stop_words())
     keras_dict = {}
-    '''
-    with open("StackOverflow_2018_Data.csv", "r") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        with open("StackOverflow_2018_TFIDF_Data.csv", "w") as csv_output:
-            appended_row = []
-            writer = csv.writer(csv_output, lineterminator='\n')
-            for row in csv_reader:
-                if row[2] == 'keras':
-                    row.append(keras_dict[row[3]])
-                    appended_row.append(row)
-                if row[2] == 'pytorch':
-                    row.append(pytorch_dict[row[3]])
-                    appended_row.append(row)
-                if row[2] == 'tensorflow':
-                    row.append(tensorflow_dict[row[3]])
-                    appended_row.append(row)
-            writer.writerows(appended_row)
-    '''
+    with open("termFrequencies.csv", "w") as csvFile:
+        writer = csv.writer(csvFile, lineterminator='\n')
+        appended_row = []
+        for ele in reversed(sorted_arr):
+            print(ele)
+            appended_row.append(ele)
+
+        writer.writerows(appended_row)
+
 if __name__ == '__main__':
     main()
